283017ab8aa82f03848df0ea8193959e72dd6728
Protik Datta <protikdatta135@gmail.com>
2026-04-02 13:49:10 +0600

    backend: auth middleware added

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies?.token;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
+    }
+
+    if (!process.env.JWT_SECRET) {
+      return res.status(500).json({ message: "Server configuration error" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found, unauthorized" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+};
+
+module.exports = authMiddleware;
